5e771752d01273865e5b0958c63641018cc7c5e7
Andrés Montoya <andresmontoyfcb@gmail.com>
2019-01-02 15:16:50 -0500

    Some bugs fixed

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,8 +1,15 @@
 <h1><?= $title ?></h1>
-<?php foreach ($posts as $post) : ?>
-  <h3><?php echo $post['title']; ?></h3>
-  <small class="post-date">Posted on: <?php echo $post['created_at']; ?> in <strong><?php echo $post['name']; ?></small>
-  <?php echo word_limiter($post['body'], 50); ?>
-  <br><br>
-  <p><a class="btn" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
+<?php foreach($posts as $post) : ?>
+	<h3><?php echo $post['title']; ?></h3>
+	<div class="row">
+		<div class="col-md-3">
+			<img width="250" height="250" src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>">
+		</div>
+		<div class="col-md-9">
+			<small class="post-date">Posted on: <?php echo $post['created_at']; ?> in <strong><?php echo $post['name']; ?></strong></small><br>
+		<?php echo word_limiter($post['body'], 60); ?>
+		<br><br>
+		<p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
+		</div>
+	</div>
 <?php endforeach; ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,38 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CIBlog</title>
-  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
-  <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
-</head>
-<body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="<?php echo base_url(); ?>">CIBlog</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarColor02">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
-        </li>
-      </ul>
+<html>
+	<head>
+		<title>ciBlog</title>
+		<link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+	</head>
+	<body>
+	<nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+      </div>
+      <div id="navbar">
+        <ul class="nav navbar-nav">
+          <li><a href="<?php echo base_url(); ?>">Home</a></li>
+          <li><a href="<?php echo base_url(); ?>about">About</a></li>
+            <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+            <li><a href="<?php echo base_url(); ?>categories">Categories</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+        <li><a href="<?php echo base_url(); ?>categories/create">Create Category</a></li>
+        </ul>
+      </div>
     </div>
   </nav>
 
